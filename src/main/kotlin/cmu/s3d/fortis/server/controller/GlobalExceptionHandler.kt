6cad2d0c63e059640acdefde4dd38a925fbccb82
Changjian Zhang <mljzcj@gmail.com>
2024-01-23 16:03:26 -0700

    Forward exception message to frontend

--- src/main/kotlin/cmu/s3d/fortis/server/controller/GlobalExceptionHandler.kt
+++ src/main/kotlin/cmu/s3d/fortis/server/controller/GlobalExceptionHandler.kt
@@ -1,13 +1,17 @@
 package cmu.s3d.fortis.server.controller
 
+import cmu.s3d.fortis.server.service.LoggingService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(
+    private val loggingService: LoggingService
+) {
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<String> {
+        loggingService.forwardFortisLog(e.stackTraceToString())
         return ResponseEntity.internalServerError().body(e.message)
     }
 }
