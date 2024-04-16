5dba20400e96f630a33bc6ed4f5552cc3c1ec0e8
Changjian Zhang <mljzcj@gmail.com>
2024-04-15 17:05:19 -0700

    Bug fix in LTS compile when the spec is a FLTL

--- src/main/kotlin/cmu/s3d/fortis/server/controller/GlobalExceptionHandler.kt
+++ src/main/kotlin/cmu/s3d/fortis/server/controller/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package cmu.s3d.fortis.server.controller
 
 import cmu.s3d.fortis.server.service.LoggingService
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class GlobalExceptionHandler(
     private val loggingService: LoggingService
 ) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<String> {
+        logger.error(e.stackTraceToString())
         loggingService.forwardFortisLog(e.stackTraceToString())
         return ResponseEntity.internalServerError().body(e.message)
     }
